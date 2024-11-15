79ac3863e760bb615a93f486b3af38316a4d4fd8
JonasCGN <jonasbo66@gmail.com>
2024-11-15 13:27:29 -0300

    att

---
+++ 1_Questao/src/functions/arv_bb/arb_bb.c
@@ -0,0 +1,25 @@
+#include "./arv_bb.h"
+
+int inserirCurso(InglesPortugues **ingles, char *palavra){
+    int insere = 1;
+    
+    if(!(*ingles)){
+        InglesPortugues *novaPalavra = (InglesPortugues*)malloc(sizeof(InglesPortugues));
+        
+        novaPalavra->palavra = palavra;
+        novaPalavra->esq = NULL;
+        novaPalavra->dir = NULL;
+
+        *ingles = novaPalavra;
+    }else{
+        if(strcmp(palavra,(*ingles)->palavra) < 0){
+            insere = inserirCurso(&(*ingles)->esq,palavra);
+        }else if(strcmp(palavra,(*ingles)->palavra) > 0){
+            insere = inserirCurso(&(*ingles)->dir,palavra);
+        }else{
+            insere = 0;
+        }
+    }
+
+    return insere;
+}
